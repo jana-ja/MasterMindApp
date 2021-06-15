4f06fcfa6ad41ce1c4b19ed13033a97b9076b757
jana-ja <janajansen@live.de>
2021-06-15 23:56:53 +0200

    - settings title - stats design und title - dimens ausgelagert - stats zurücksetzen - Stats pojo verwendet - status bar color schwarz

--- app/src/main/java/com/example/mastermind/model/Stats.java
+++ app/src/main/java/com/example/mastermind/model/Stats.java
@@ -1,13 +1,22 @@
 package com.example.mastermind.model;
 
+import android.content.Context;
+
+import com.example.mastermind.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stats {
 
     private int numberWon;
     private int numberLost;
     private int numberQuit;
 
+
     //TODO infos zu den einstellung (schwierigkeit) speichern?
 
+    //TODO das hier richtig machen
 
     public Stats() {
     }
@@ -40,5 +49,16 @@ public class Stats {
 
     public void setNumberQuit(int numberQuit) {
         this.numberQuit = numberQuit;
+    }
+
+    public static List<String> getAllStatsKeys(Context context){
+        ArrayList<String> allKeys = new ArrayList<>();
+
+        allKeys.add(context.getString(R.string.stats_started_key));
+        allKeys.add(context.getString(R.string.stats_won_key));
+        allKeys.add(context.getString(R.string.stats_lost_key));
+
+        return allKeys;
+
     }
 }
--- app/src/main/java/com/example/mastermind/view/StatsActivity.java
+++ app/src/main/java/com/example/mastermind/view/StatsActivity.java
@@ -1,18 +1,26 @@
 package com.example.mastermind.view;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mastermind.R;
+import com.example.mastermind.model.Stats;
 
-public class StatsActivity extends AppCompatActivity {
+import java.util.List;
+
+public class StatsActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     TextView valueStarted, textWon, textLost, textQuit;
+    Button btnResetStats;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,13 +32,41 @@ public class StatsActivity extends AppCompatActivity {
         this.textLost = findViewById(R.id.value_lost);
         this.textQuit = findViewById(R.id.value_quit);
 
+        this.btnResetStats = findViewById(R.id.button_reset_stats);
+        this.btnResetStats.setOnClickListener(v -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);//TODO hard coded strings
+                builder.setMessage("Wirklich die Statistiken löschen?").setPositiveButton("Ja", this)
+                        .setNegativeButton("Nein", this).show();
+        });
+
+        loadStats();
+
+
+
+    }
+
+    private void resetStats() {
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.stats_preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        List<String> allStatsKeys = Stats.getAllStatsKeys(this);
+        allStatsKeys.forEach(key -> editor.putInt(key, 0));
+        editor.apply();
+
+        loadStats();
+    }
+
+    private void loadStats() {
         SharedPreferences sharedPref = getSharedPreferences(
                 getString(R.string.stats_preference_file_key), Context.MODE_PRIVATE);
 
+        // TODO auch das stats objekt verwenden? ne glaube nicht kp
         int numberStarted = sharedPref.getInt(getString(R.string.stats_started_key), 0);
         int numberWon = sharedPref.getInt(getString(R.string.stats_won_key), 0);
         int numberLost = sharedPref.getInt(getString(R.string.stats_lost_key), 0);
-        int numberQuit = sharedPref.getInt(getString(R.string.stats_quit_key), 0);
+//        int numberQuit = sharedPref.getInt(getString(R.string.stats_quit_key), 0);
+        int numberQuit = numberStarted - numberWon - numberLost;
 
         //TODO hat Stats objekt einen nutzen?
 
@@ -41,12 +77,25 @@ public class StatsActivity extends AppCompatActivity {
 
         //TODO etwas zum stats resetten
         //TODO schön machen
-        //TODO stats gestartete games
-        //TODO beendete games
+        //TODO durchschnittliche versuche gebraucht bei gewonnen
 
+        //TODO stats pro spielmodus??
+        //TODO warum gridlayout nicht vertikale trennung in der mitte????
 
 
     }
 
 
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                resetStats();
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                //No button clicked
+                break;
+        }
+    }
 }
