d2be3aa5d27f0bf6fad9c88a6ffed7a80aac3a7e
JanaJa <janajansen@live.de>
2022-04-07 02:00:20 +0200

    remove deprecated pin colors

--- app/src/main/java/com/example/mastermind/model/PinColor.java
+++ app/src/main/java/com/example/mastermind/model/PinColor.java
@@ -10,30 +10,6 @@ import com.example.mastermind.R;
 public enum PinColor {
     RED, ORANGE, YELLOW, GREEN, BLUE, PINK, GREY, WHITE, EMPTY, SOLUTION;
 
-    public int getColor(Context context){
-        switch(this){
-            case BLUE:
-                return ContextCompat.getColor(context, R.color.blue_pin);
-            case GREEN:
-                return ContextCompat.getColor(context, R.color.green_pin);
-            case ORANGE:
-                return ContextCompat.getColor(context, R.color.orange_pin);
-            case PINK:
-                return ContextCompat.getColor(context, R.color.pink_pin);
-            case RED:
-                return ContextCompat.getColor(context, R.color.red_pin);
-            case WHITE:
-                return ContextCompat.getColor(context, R.color.white_pin);
-            case GREY:
-                return ContextCompat.getColor(context, R.color.grey_pin);
-            case YELLOW:
-                return ContextCompat.getColor(context, R.color.yellow_pin);
-            case EMPTY:
-                return ContextCompat.getColor(context, R.color.empty_pin);
-            default:
-                return Color.TRANSPARENT;
-        }
-    }
 
     public int getImageName(){
         switch(this){
